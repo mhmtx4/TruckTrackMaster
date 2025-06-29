b0ffa149a49835d7634ecb1675181c2c6997669a
Mehmet <71027250+mhmtx4@users.noreply.github.com>
2025-06-29 19:21:06 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: 'client', // <--- Burası önemli!
   plugins: [react()],
+  build: {
+    outDir: '../dist', // build çıktısı kök dist'e çıksın
+    emptyOutDir: true,
+  },
 });
